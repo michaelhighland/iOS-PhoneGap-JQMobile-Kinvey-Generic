86266b57f50820cab082cdea610c4412e30c1bb7
Michael Highland <michael@iamfutureproof.com>
2012-12-03 13:55:58 -0500

    Finished

--- www/js/app.js
+++ www/js/app.js
@@ -19,7 +19,9 @@
 var app = {
     // Application Constructor
     initialize: function() {
-        this.bindEvents();
+        this.currentDevice = null;
+		this.bindEvents();
+	
     },
     // Bind Event Listeners
     //
@@ -29,9 +31,6 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
 		console.log("Device Ready");
 		kinveyInit();
@@ -40,10 +39,18 @@ var app = {
     }
 };
 
+function registerDevice(){
+	
+	
+	
+}
+
+
+
 function kinveyInit(){
 		Kinvey.init({
 	        'appKey': '<app id>',
-	        'appSecret': '<app key>'
+	        'appSecret': '<app secret>'
 	    });
 }
 
