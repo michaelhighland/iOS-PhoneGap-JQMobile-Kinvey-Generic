225df96618515d757ea53ed3699855d38b982a37
Michael Highland <michael@iamfutureproof.com>
2012-12-03 13:29:22 -0500

    JQMobile configured

--- www/js/app.js
+++ www/js/app.js
@@ -1,58 +1,59 @@
-function App(){
-	this.setup = function(){
-		checkIn();
-	}
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicity call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+		console.log("Device Ready");
+		kinveyInit();
+		kinveyPing();
+		
+    }
+};
+
+function kinveyInit(){
+		Kinvey.init({
+	        'appKey': '<app id>',
+	        'appSecret': '<app key>'
+	    });
 }
 
-function initKinvey(){
-	Kinvey.init({
-		appKey: 'kid_PTU06lpJ9f',
-		appSecret: '06decabf816043728dbe15c8df5d2662'
+function kinveyPing(){
+	Kinvey.ping({
+	  success: function(response) {
+	    console.log('Kinvey Ping Success. Kinvey Service is alive, version: ' + response.version + ', response: ' + response.kinvey);
+	  },
+	  error: function(error) {
+	    console.log('Kinvey Ping Failed. Response: ' + error.description);
+	  }
 	});
-}
-
-function pingKinvey(){
-	
- 	Kinvey.ping({
-	    success: function(response) {
-			console.log('Kinvey Service is alive, version: ' + response.version + ', response: ' + response.kinvey);
-			return true;
-	    },
-	    error: function(error) {
-			console.log('Kinvey Ping Failed. Response: ' + error.description);
-			return false;
-	    }
-	 });
-}
-
-function checkIn(){
-	
-	console.log("Attempting Checkin for: "+device.uuid);
-	
-	var user = new Kinvey.User();
-	user.login(device.uuid, device.uuid, {
-	    success: function(user) {
-	        $("#test").html("Device Recognized");
-	    },
-	    error: function(error) {
-	        registerDevice();
-	    }
-	});
-	
-}
-
-function registerDevice(){
-	// Create a user named John Doe.
-	Kinvey.User.create({
-	    username: device.uuid,
-	    password: device.uuid,
-	}, {
-	    success: function(user) {
-	        $("#test").html("Device Registered to "+device.uuid);
-	    },
-	    error: function(error) {
-	        $("#test").html("Could not register device");
-	    }
-	});
-	
 }
